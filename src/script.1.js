b65e7f90131f94fd8945b7a23434e4e646ae5bbb
goodmorningnelson <mtr.ruehr@gmail.com>
2016-11-24 09:39:13 -0500

    jade not working

---
+++ src/script.1.js
@@ -0,0 +1,2 @@
+require('./css/test.scss');
+require('../index.jade');
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  //define entry point
+  entry: './src/app.js',
+
+  //define output point
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.jade$/,
+        loader: 'jade'
+      }
+    ]
+  }
+};
